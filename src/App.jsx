ed11c040724237a5fe5c60724e607bb0dff854a9
jtjemzz09 <jtjemzz09@LAPTOP-292I2KQV>
2023-07-31 22:50:42 -0700

    added home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,13 @@ export default function App(){
 return(
 <div id="container">
       <div id="navbar"> 
-      <Link to="/blue">blue</Link>
-      <Link to="/red">red</Link>
+      <Link to="/">Home</Link>
+      <Link to="/blue">Blue</Link>
+      <Link to="/red">Red</Link>
       </div>
       <div id="main-section">
         <Routes>
-          <Route path ="/" element ={}/>
+          <Route path ="/" element ={<Home/>}/>
           <Route path="/blue" element= {<Blue/>}/>
           <Route path="/red" element= {<Red/>}/>
         </Routes>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home(){
 
     return(
 
-       <div>
+       <div className="home">
         <h1> Home</h1>
        </div> 
     )
